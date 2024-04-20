1011dfe97d851f98c6bb48ff7b98471ec172dcd2
Dantetisso <dantetisoriero@gmail.com>
2024-04-19 22:32:58 -0300

    salto prototipado añadido falta meter bool

---
+++ Sistemas_Parcial_1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int speed;
+    [SerializeField] private int jumpForce;
+    [SerializeField] private int damage;
+    [SerializeField] private Transform playerTransform;
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerTransform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerTransform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        
+
+    }
+}
