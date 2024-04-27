34762a0e71216e3ebc24280e339d073048a7cec3
Dantetisso <dantetisoriero@gmail.com>
2024-04-27 05:01:37 -0300

    salto a lo hollow knight

--- Sistemas_Parcial_1/Assets/Scripts/PlayerController.cs
+++ Sistemas_Parcial_1/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     private float xInput;
     private float YInput;
+
+    [Header("Movement")]
     [SerializeField] private float maxSpeed;
     [SerializeField] private float maxAcceleration;
-    [SerializeField] private int jumpForce;
-    [SerializeField] private int damage;
+    [SerializeField] private int jumpSpeed;
+    [SerializeField] private int extraJumps;
     [SerializeField] private Transform feet;
-    [SerializeField] private LayerMask layer;
+    [SerializeField] private LayerMask groundLayer;
+
     private bool IsJumping;
     private Rigidbody2D rb;
-   
+    private int jumpCount;
+
+    [Header("Combat")]
+    [SerializeField] private int damage;
 
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+       
     }
 
 
@@ -28,17 +36,37 @@ public class PlayerController : MonoBehaviour
         xInput = Input.GetAxisRaw("Horizontal");
         YInput = Input.GetAxisRaw("Vertical");
 
+        Collider2D ground = Physics2D.OverlapBox(feet.position, new Vector2(0.99f, 0.05f), 0f, groundLayer);
+        bool IsGrounded = (ground != null);
         
         if (Input.GetKeyDown(KeyCode.W))
         {
-            Collider2D ground = Physics2D.OverlapBox(feet.position, Vector2.one, 0f, layer);
-            print(ground.gameObject.name);
-
-            if (ground != null)
+            if (IsGrounded)
             {
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                Jump();
+            }
+            else
+            {
+                if (jumpCount > 0) 
+                {
+                    Jump();
+                    jumpCount--;
+                }
+            }         
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            if (rb.velocity.y > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
             }
             
+        }
+
+        if (IsGrounded)
+        {
+            jumpCount = extraJumps;
         }
     }
 
@@ -48,14 +76,21 @@ public class PlayerController : MonoBehaviour
 
        float diffSpeed = desiredSpeed - rb.velocity.x;
 
-        diffSpeed = Mathf.Clamp(diffSpeed, -maxAcceleration, maxAcceleration);
+       diffSpeed = Mathf.Clamp(diffSpeed, -maxAcceleration, maxAcceleration);
 
-        float Xforce = rb.mass * diffSpeed;
+       float Xforce = rb.mass * diffSpeed;
 
         if (xInput > 0.1f|| xInput < -0.1f)
         {
             rb.AddForce(new Vector2(Xforce, 0f), ForceMode2D.Impulse);
         }       
     }
+
+
+    private void Jump()
+    {        
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+    }
+
 
 }
